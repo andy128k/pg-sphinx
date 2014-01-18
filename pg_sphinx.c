4dbefc755317eb934fdf4645d3fe4118277ed28e
Andrey Kutejko <andy128k@gmail.com>
2014-01-18 10:31:28 +0200

    more null checks

--- pg_sphinx.c
+++ pg_sphinx.c
@@ -28,33 +28,33 @@ Datum pg_sphinx_delete(PG_FUNCTION_ARGS);
 PG_FUNCTION_INFO_V1(pg_sphinx_snippet);
 Datum pg_sphinx_snippet(PG_FUNCTION_ARGS);
 
-static void to_pstring(PString *pstr, Datum d, bool isnull)
-{
-  if (isnull)
-    {
-      pstr->str = NULL;
-      pstr->len = 0;
-    }
-  else
-    {
-      pstr->str = VARDATA(d);
-      pstr->len = VARSIZE(d) - VARHDRSZ;
-    }
-}
+#define TO_PSTRING(pstr, d, isnull)             \
+  do {                                          \
+    if (isnull)                                 \
+      {                                         \
+        pstr.str = NULL;                        \
+        pstr.len = 0;                           \
+      }                                         \
+    else                                        \
+      {                                         \
+        pstr.str = VARDATA(d);                  \
+        pstr.len = VARSIZE(d) - VARHDRSZ;       \
+      }                                         \
+  } while(0)
 
-static void varchar_to_pstring(PString *pstr, VarChar *d, bool isnull)
-{
-  if (isnull)
-    {
-      pstr->str = NULL;
-      pstr->len = 0;
-    }
-  else
-    {
-      pstr->str = VARDATA(d);
-      pstr->len = VARSIZE(d) - VARHDRSZ;
-    }
-}
+#define VARCHAR_TO_PSTRING(pstr, d, isnull)     \
+  do {                                          \
+    if (isnull)                                 \
+      {                                         \
+        pstr.str = NULL;                        \
+        pstr.len = 0;                           \
+      }                                         \
+    else                                        \
+      {                                         \
+        pstr.str = VARDATA(d);                  \
+        pstr.len = VARSIZE(d) - VARHDRSZ;       \
+      }                                         \
+  } while(0)
 
 Datum pg_sphinx_select(PG_FUNCTION_ARGS)
 {
@@ -77,13 +77,13 @@ Datum pg_sphinx_select(PG_FUNCTION_ARGS)
         {
           PString index = {0, 0}, query = {0, 0}, condition = {0, 0}, order = {0, 0}, options = {0, 0};
           int offset, limit;
-          to_pstring(&index,     PG_GETARG_DATUM(0), PG_ARGISNULL(0));
-          to_pstring(&query,     PG_GETARG_DATUM(1), PG_ARGISNULL(1));
-          to_pstring(&condition, PG_GETARG_DATUM(2), PG_ARGISNULL(2));
-          to_pstring(&order,     PG_GETARG_DATUM(3), PG_ARGISNULL(3));
+          TO_PSTRING(index,     PG_GETARG_DATUM(0), 0);
+          TO_PSTRING(query,     PG_GETARG_DATUM(1), 0);
+          TO_PSTRING(condition, PG_GETARG_DATUM(2), PG_ARGISNULL(2));
+          TO_PSTRING(order,     PG_GETARG_DATUM(3), PG_ARGISNULL(3));
           offset = PG_ARGISNULL(4) ?  0 : PG_GETARG_UINT32(4);
           limit  = PG_ARGISNULL(5) ? 20 : PG_GETARG_UINT32(5);
-          to_pstring(&options,   PG_GETARG_DATUM(6), PG_ARGISNULL(6));
+          TO_PSTRING(options,   PG_GETARG_DATUM(6), PG_ARGISNULL(6));
 
           funcctx->user_fctx = sphinx_select(&index, &query, &condition, &order, offset, limit, &options);
         }
@@ -129,7 +129,7 @@ Datum pg_sphinx_replace(PG_FUNCTION_ARGS)
   if (PG_ARGISNULL(0) || PG_ARGISNULL(1) || PG_ARGISNULL(2))
     PG_RETURN_VOID();
 
-  to_pstring(&index, PG_GETARG_DATUM(0), PG_ARGISNULL(0));
+  TO_PSTRING(index, PG_GETARG_DATUM(0), 0);
   id = PG_GETARG_UINT32(1);
   input = PG_GETARG_ARRAYTYPE_P(2);
 
@@ -148,9 +148,9 @@ Datum pg_sphinx_replace(PG_FUNCTION_ARGS)
   for (; array_iterate(iter, &value, &isnull); ++i)
     {
       if (i % 2 == 0)
-        to_pstring(&columns[i / 2], value, isnull);
+        TO_PSTRING(columns[i / 2], value, isnull);
       else
-        to_pstring(&values[i / 2], value, isnull);
+        TO_PSTRING(values[i / 2], value, isnull);
     }
   array_free_iterator(iter);
 
@@ -170,7 +170,7 @@ Datum pg_sphinx_delete(PG_FUNCTION_ARGS)
   if (PG_ARGISNULL(0) || PG_ARGISNULL(1))
     PG_RETURN_VOID();
 
-  to_pstring(&index, PG_GETARG_DATUM(0), PG_ARGISNULL(0));
+  TO_PSTRING(index, PG_GETARG_DATUM(0), 0);
   id = PG_GETARG_UINT32(1);
 
   sphinx_delete(&index, id);
@@ -198,15 +198,18 @@ Datum pg_sphinx_snippet(PG_FUNCTION_ARGS)
   if (PG_ARGISNULL(0) || PG_ARGISNULL(1) || PG_ARGISNULL(2))
     PG_RETURN_NULL();
 
-  varchar_to_pstring(&index,        PG_GETARG_VARCHAR_P(0), PG_ARGISNULL(0));
-  varchar_to_pstring(&match,        PG_GETARG_VARCHAR_P(1), PG_ARGISNULL(1));
-  varchar_to_pstring(&data,         PG_GETARG_VARCHAR_P(2), PG_ARGISNULL(2));
-  varchar_to_pstring(&before_match, PG_GETARG_VARCHAR_P(3), PG_ARGISNULL(3));
-  varchar_to_pstring(&after_match,  PG_GETARG_VARCHAR_P(4), PG_ARGISNULL(4));
+  VARCHAR_TO_PSTRING(index,        PG_GETARG_VARCHAR_P(0), 0);
+  VARCHAR_TO_PSTRING(match,        PG_GETARG_VARCHAR_P(1), 0);
+  VARCHAR_TO_PSTRING(data,         PG_GETARG_VARCHAR_P(2), 0);
+  VARCHAR_TO_PSTRING(before_match, PG_GETARG_VARCHAR_P(3), PG_ARGISNULL(3));
+  VARCHAR_TO_PSTRING(after_match,  PG_GETARG_VARCHAR_P(4), PG_ARGISNULL(4));
 
   sphinx_snippet(&index, &match, &data, &before_match, &after_match,
                  return_text, &result_text);
 
-  PG_RETURN_TEXT_P(result_text);
+  if (result_text)
+    PG_RETURN_TEXT_P(result_text);
+  else
+    PG_RETURN_NULL();
 }
 
